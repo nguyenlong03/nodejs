5f1d80205dc587275342dd9a7ac605d14753a74d
nguyenlong03 <nguyenlong682003@gmail.com>
2025-11-06 17:47:47 +0700

    feat : logic map fillter reduce

--- Day2/Testing.js
+++ Day2/Testing.js
@@ -1,1 +1,31 @@
+const map1 = (arr , callback)=>{
+  const result = [];
+  for(let i = 0; i < arr.length; i++){
+    result.push(callback(arr[i] , i , arr));
+  }
+  return result;
+}
+const reduce1 = (arr , callback , initialValue)=>{
+  let accumulator = initialValue;
+    for(let i = 0; i < arr.length; i++){
+        accumulator = callback(accumulator , arr[i]);
+    }
+    return accumulator;
+}
 
+
+const filter1 = (arr , callback)=>{
+    let resuts = []
+    for (let i = 0; i < arr.length; i++) {
+    if(callback(arr[i] , i , arr)){
+        resuts.push(arr[i] )
+    }
+     
+    }
+    return resuts   
+
+}
+
+const manga = [1,2,3,4,5,5,6,6]
+const newmanga = filter1(manga,(item)=>item < 4)
+console.log(newmanga);
